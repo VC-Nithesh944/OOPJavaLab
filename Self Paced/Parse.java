7cbdf7e54c47f244b0c08d39463a23e32ad214b4
Nithesh <nitheshu944@gmail.com>
2024-10-19 19:04:48 +0530

    Added A code to Parse String using Generics

---
+++ Self Paced/Parse.java
@@ -0,0 +1,10 @@
+public class Parse {
+    public static void main(String[] args) {
+        String a = "20";
+        String b = "23";
+        int p = Integer.parseInt(a);
+        int q = Integer.parseInt(b);
+        System.out.println(p*q);    //460
+        //System.out.println(a*b);    Error
+    }
+}
