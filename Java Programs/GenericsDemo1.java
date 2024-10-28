a79d9c3c8c3e9573ca8e0c22862f12da979398f7
Nithesh <nitheshu944@gmail.com>
2024-10-28 18:39:09 +0530

    Added Terminal image for Generics1

--- Java Programs/GenericsDemo1.java
+++ Java Programs/GenericsDemo1.java
@@ -10,12 +10,13 @@ class GenericsDemo1 {
         i.showType();
 
         Generic1<Float> f = new Generic1<Float>(43.2F);
-        System.out.println("The Auto boxed value is : "+ f.getobj());
+        System.out.println("\nThe Auto boxed value is : "+ f.getobj());
 
         float num3 = f.getobj();
         double num4 = f.getobj();
         System.out.println("The Auto Unboxed value in float is : "+num3+ " and typecasted to double is: " + num4);
 
+        f.showType();
     }
 }
 
