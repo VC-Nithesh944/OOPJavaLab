a923227ff23435528cf9200819eddd1be7af3335
Nithesh <163291761+VC-Nithesh944@users.noreply.github.com>
2024-11-11 15:26:10 +0530

    Created D.java

---
+++ Java Programs/PackageClass/p2/D.java
@@ -0,0 +1,10 @@
+package p2;
+
+public class D {
+    public D(){
+        System.out.println("Class D constructor");
+    }
+    public void display(){
+        System.out.println("Display method D");
+    }
+}
