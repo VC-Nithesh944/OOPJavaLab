14d6275897b092b6d1b33438d74392bb0c7e9847
Nithesh <163291761+VC-Nithesh944@users.noreply.github.com>
2024-11-11 15:24:55 +0530

    Create B.java

---
+++ Java Programs/PackageClass/p1/B.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class B {
+    public B(){
+        System.out.println("Class B constructor");
+    }
+    public void display(){
+        System.out.println("Display method B");
+    }
+}
