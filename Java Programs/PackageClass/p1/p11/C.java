c7dc093a8dc27725b01db0947e2fe6a566896837
Nithesh <163291761+VC-Nithesh944@users.noreply.github.com>
2024-11-11 15:25:31 +0530

    Created C.java

---
+++ Java Programs/PackageClass/p1/p11/C.java
@@ -0,0 +1,10 @@
+package p1.p11;
+
+public class C {
+    public C(){
+        System.out.println("CLass C constructor");
+    }
+    public void display(){
+        System.out.println("Display method C");
+    }
+}
