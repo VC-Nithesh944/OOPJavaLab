a1186f8bc481bd9340f1760c69cb8da123b405be
Nithesh <163291761+VC-Nithesh944@users.noreply.github.com>
2024-11-11 15:24:04 +0530

    Created PackTest.java

---
+++ Java Programs/PackageClass/Pack/PackTest.java
@@ -0,0 +1,24 @@
+package Pack;
+
+import p1.*;
+import p1.p11.*;
+import p2.*;
+
+
+public class PackTest {
+    public static void main(String args[]){
+        A a1 = new A();
+        B b1 = new B();
+        C c1 = new C();
+        D d1 = new D();
+
+        a1.display();
+        
+        b1.display();
+        
+        c1.display();
+        
+        d1.display();
+        
+    }
+}
